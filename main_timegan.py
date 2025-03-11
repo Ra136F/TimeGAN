c45d09ce35cd70a2b7406bf025714498dfaa6090
lxf <111@qq.com>
2025-03-11 15:21:06 +0800

    结果保存优化

--- main_timegan.py
+++ main_timegan.py
@@ -189,7 +189,7 @@ if __name__ == '__main__':
   parser.add_argument(
       '--iteration',
       help='Training iterations (should be optimized)',
-      default=5000,
+      default=10000,
       type=int)
   parser.add_argument(
       '--batch_size',
